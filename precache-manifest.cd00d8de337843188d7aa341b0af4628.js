f8dbda3733ce57be77dbb668b926f20aab65db86
Vishal Kumar <kumar.preparestudy@outlook.com>
2020-09-28 12:18:47 +0530

    Updates

---
+++ precache-manifest.cd00d8de337843188d7aa341b0af4628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20c907f936c8eea7824ed5258fd4546",
+    "url": "/studentapp/index.html"
+  },
+  {
+    "revision": "874338edbde471462f5c",
+    "url": "/studentapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "824b51940cb5ba0bfb99",
+    "url": "/studentapp/static/js/2.3412aa44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/studentapp/static/js/2.3412aa44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874338edbde471462f5c",
+    "url": "/studentapp/static/js/main.2a361cd2.chunk.js"
+  },
+  {
+    "revision": "97ab76475d9775b9bb45",
+    "url": "/studentapp/static/js/runtime-main.39c8c1c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/studentapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
